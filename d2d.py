cd8ff9d553753543372bd5a4fbd72516ce2b5444
wlswotmd <umsun2357@korea.ac.kr>
2025-03-11 19:16:50 +0900

    fix: change to work with latest gef

--- d2d.py
+++ d2d.py
@@ -22,7 +22,8 @@ if is_gdb:
         GEFClient(register_external_context_pane, gef_print, gef)
     elif "__gef__" in _globals:
         from decomp2dbg.clients.gdb.gef_bata24_client import GEFBata24Client
-        GEFBata24Client(gef_print, get_process_maps, get_filepath, ContextCommand, __config__, __gef__)
+
+        GEFBata24Client(gef_print, AddressUtil.get_codebase, __gef_command_instances__, Config)
     elif "pwndbg" in _globals:
         from decomp2dbg.clients.gdb.pwndbg_client import PwndbgClient
         PwndbgClient()
